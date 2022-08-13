e72d39c3161e8f3c419c3f6ff832bcdfddf72421
Jayantagit <jayantamandal2006@gmail.com>
2022-08-13 20:39:48 +0530

    kk

--- Day6/Car.js
+++ Day6/Car.js
@@ -11,10 +11,18 @@ class CarSample//Class is Blue Print with Properties and behaviour-Objects of th
         return this.price;
 
     }
+    showDetails()
+    {
+        console.log("Detail value is"+this.color+":"+this.price);
+    }
 
 }
 const c1=new CarSample("Green",1000);
+const c2=new CarSample("Yellow",2000);
 console.log(c1.color);
 console.log(c1.getPrice());
+console.log(c2.color);
 //Using Class we can define multiple Objects.At the Time Of Object creation constructor construct the Objet
+//ie helps to Define the Object
 //Object is a collection of Properties and Behavior
+c1.showDetails();
---
+++ Day6/Person.js
@@ -0,0 +1,19 @@
+class Person
+{
+
+    constructor(name,age,location)
+    {
+        this.name=name;
+        this.age=age;
+        this.location=location;
+    }
+
+
+
+
+}
+
+let manoj=new Person("Manoj",10,"Kol");
+let joy=new Person();
+
+console.log(manoj.location);
